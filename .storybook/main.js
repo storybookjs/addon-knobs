2d258bf1381eb5ec5c712062b293074410ad02e3
Benjamin Kindle <benjaminkindle@yahoo.com>
2023-03-11 13:02:18 -0500

    remove extra addon

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: ["../preset", "@storybook/addon-essentials"],
+  addons: ["../preset"],
   framework: {
     name: "@storybook/react-webpack5",
     options: {}
