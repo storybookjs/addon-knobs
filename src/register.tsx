844f5a21f580aae4e65883e78c9b9177fada6eba
Benjamin Kindle <benjaminkindle@yahoo.com>
2024-03-02 10:42:41 -0500

    feat: storybook 8 support WIP

--- src/register.tsx
+++ src/register.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react';
-import { addons } from '@storybook/addons';
+import { addons, types } from '@storybook/manager-api';
 import Panel from './components/Panel';
 import { ADDON_ID, PANEL_ID, PARAM_KEY } from './shared';
 import { createTitleListener } from './title';
 
 addons.register(ADDON_ID, (api) => {
-  addons.addPanel(PANEL_ID, {
+  addons.add(PANEL_ID, {
+    type: types.PANEL,
     title: createTitleListener(api),
-    render: ({ active, key }) => <Panel api={api} key={key} active={active} />,
+    render: ({ active }) => <Panel api={api} active={active} />,
     paramKey: PARAM_KEY,
   });
 });
